4b753bdd7b3713e348b66c3cd0eb4f271bdf5a99
Alan Zheng <89716079+VeryAmazed@users.noreply.github.com>
2023-05-01 00:55:32 -0400

    Update stdin_Template.cpp
    Added a point update seg tree taken from USACO.guide and then modified by me

--- stdin_Template.cpp
+++ stdin_Template.cpp
@@ -227,6 +227,65 @@ for(int i =0; i < n; i++){
 	poly1[i+1] = ((poly1[i]*P1)%M1 + str[i])%M1;
 	poly2[i+1] = ((poly2[i]*P2)%M2 + str[i])%M2;
 }
+
+// Taken from USACO.guide and then modified by me
+/** A data structure that can answer point update & range minimum queries. */
+template <class T> 
+struct SegTree {
+	/** The operation to use for combining two elements. (Must be associative) 
+		Used for querying (when the parent combines its two children)
+	 */
+	 // change as necessary
+	T query_comb(T a, T b) { 
+		return a + b;
+	}
+	
+	// operation for updating element at the specified index
+	// change as necessary
+	T update_comb(T a, T b) {
+		return a + b;
+	}
+	
+	const T DEFAULT = 0;  // Default value, change as necessary
+
+	vector<T> segtree;
+	int len;
+
+	SegTree(int len) : len(len), segtree(len * 2, DEFAULT) {}
+	
+	// look at ASSERTS to see how you should be indexing things
+	
+	/** Sets the value at ind to val. */
+	void set(int ind, T val) {
+		// assert(0 <= ind && ind < len);
+		ind += len;
+		segtree[ind] = val;
+		for (; ind > 1; ind /= 2) {
+			segtree[ind >> 1] = query_comb(segtree[ind], segtree[ind ^ 1]);
+		}
+	}
+	
+	/** updates the value at ind to val. */
+	void update(int ind, T val) {
+		// assert(0 <= ind && ind < len);
+		ind += len;
+		segtree[ind] = update_comb(segtree[ind], val);
+		for (; ind > 1; ind /= 2) {
+			segtree[ind >> 1] = query_comb(segtree[ind], segtree[ind ^ 1]);
+		}
+	}
+
+	/** queries the range [start, end) */
+	T query(int start, int end) {
+		// assert(0 <= start && start < len && 0 < end && end <= len);
+		T sum = DEFAULT;
+		for (start += len, end += len; start < end; start /= 2, end /= 2) {
+			if ((start & 1) != 0) { sum = query_comb(sum, segtree[start++]); }
+			if ((end & 1) != 0) { sum = query_comb(sum, segtree[--end]); }
+		}
+		return sum;
+	}
+};
 	
 // monkey8's Lazy Prop Seg Tree Template
 // Modified by Me
