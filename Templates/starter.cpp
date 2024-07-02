9c6ef2708bd51fa8129aec1078499bc91ba44c34
Alan Zheng <89716079+VeryAmazed@users.noreply.github.com>
2024-07-02 13:44:33 -0400

    Update starter.cpp

--- Templates/starter.cpp
+++ Templates/starter.cpp
@@ -61,6 +61,7 @@ int main() {
 	// Plan out the steps in words on a piece of paper before implementing
 	// after RTE(obviously) but also WA, run valgrind!!!
 	// Testing your solution on samples before coding is a great way to see if you read the problem correctly!!!
+	// Also take notes about key elements in the problem statement while reading the problem!!!
 	
 	//cout << fixed << setprecision(12);
 	// if you use ld, use the above and don't use string stream
