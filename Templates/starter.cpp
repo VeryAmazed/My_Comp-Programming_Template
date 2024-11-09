dee429a4f4f19bfbaf9629b8d307e5a6f7539066
Alan Zheng <89716079+VeryAmazed@users.noreply.github.com>
2024-11-09 13:03:00 -0500

    Update starter.cpp

--- Templates/starter.cpp
+++ Templates/starter.cpp
@@ -8,12 +8,12 @@ typedef long double ld;
 // contrsuct umaps like this, unordered_map<long long, int, custom_hash> safe_map;
 // FIXED_RANDOM is static so it doesn not get redeclared between function calls
 struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
+	static uint64_t splitmix64(uint64_t x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
     }
 
     size_t operator()(uint64_t x) const {
@@ -52,7 +52,7 @@ int main() {
 	// use this if you read in from a file
 	/*
 	freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+  freopen("out.txt", "w", stdout);
 	*/
 	
 	stringstream ss;
